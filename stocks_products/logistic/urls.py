c30e9de53b884f5ffe2edeb62d5439851b9a9a0a
Alex55550000 <106884796+Alex55550000@users.noreply.github.com>
2023-06-08 22:57:14 +0300

    Delete stocks_products directory

--- stocks_products/logistic/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import ProductViewSet, StockViewSet
-
-router = DefaultRouter()
-router.register('products', ProductViewSet)
-router.register('stocks', StockViewSet)
-
-urlpatterns = router.urls
